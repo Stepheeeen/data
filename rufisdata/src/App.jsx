9da773ac8e8d11b5ef0392b9bb41b6982b27e061
Lkjhhgffdssaaa <Stephenon56@gmail.com>
2023-09-04 23:54:41 +0100

    final commit

--- rufisdata/src/App.jsx
+++ rufisdata/src/App.jsx
@@ -50,7 +50,7 @@ function App() {
         </ul>
 
         <button className='nav-btn close' onClick={showNavbar}>
-          X
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="27" height="27"><path d="M7.82843 10.9999H20V12.9999H7.82843L13.1924 18.3638L11.7782 19.778L4 11.9999L11.7782 4.22168L13.1924 5.63589L7.82843 10.9999Z" fill="rgba(0,0,0,1)"></path></svg>
         </button>
       </nav>
 
